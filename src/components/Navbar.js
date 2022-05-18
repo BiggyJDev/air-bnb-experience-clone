5894830abc1f1cb65bcdf19357874575de1c9662
Jordan Wilson <biggyj_dev@outlook.com>
2022-05-18 20:54:15 +0100

    Navbar component complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <img className='nav--logo' src='./air-bnb-logo.png' alt='Air BnB Logo' />
+    </div>
+  );
+}
+
+export default Navbar;
